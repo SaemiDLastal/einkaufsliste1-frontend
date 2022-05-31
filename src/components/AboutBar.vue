0b4bbbde0cd8e8b8f8dae172978980db9a578861
SaemiDLastal <104317393+SaemiDLastal@users.noreply.github.com>
2022-05-31 16:18:08 +0200

    first commit

---
+++ src/components/AboutBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Aboutbar"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ShoppingCards.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Welcome to your shopping cards</h1>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingCards'
+}
+</script>
+
+<style scoped>
+
+</style>
